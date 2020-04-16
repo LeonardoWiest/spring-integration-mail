07967bd45e682ba399bb17808ee137dc26b9dcf9
Leonardo Wiest <leonardowiest@hotmail.com>
2020-04-16 19:39:19 -0300

    Doc: Prototipagem

--- src/main/java/com/github/leonardowiest/Application.java
+++ src/main/java/com/github/leonardowiest/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringIntegrationMailApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringIntegrationMailApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ src/main/java/com/github/leonardowiest/config/IntegrationFlowConfig.java
@@ -0,0 +1,10 @@
+package com.github.leonardowiest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.config.EnableIntegration;
+
+@EnableIntegration
+@Configuration
+public class IntegrationFlowConfig {
+
+}
