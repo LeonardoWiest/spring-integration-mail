041a3a4d92006f78cf332cfaeb7645d9d33107b2
Leonardo Wiest <leonardowiest@hotmail.com>
2020-04-28 19:01:11 -0300

    Doc: Prototipagem

--- src/main/java/com/github/leonardowiest/service/MailReceivingService.java
+++ src/main/java/com/github/leonardowiest/service/MailReceivingService.java
@@ -1,5 +1,14 @@
 package com.github.leonardowiest.service;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Part;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
@@ -16,8 +25,32 @@ public class MailReceivingService {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	public void handle(MimeMessage message) {
+	public void handle(MimeMessage message) throws MessagingException, IOException {
 
-		logger.info("Entrou...");
+		Folder folder = message.getFolder();
+
+		folder.open(Folder.READ_ONLY);
+
+		Message[] messages = folder.getMessages();
+
+		for (int index = 0; index < messages.length; index++) {
+
+			Multipart multiPart = (Multipart) message.getContent();
+
+			Integer numberOfParts = multiPart.getCount();
+
+			for (int partCount = 0; partCount < numberOfParts; partCount++) {
+
+				MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
+
+				if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
+
+					String fileName = part.getFileName();
+
+					part.saveFile("C:\\Ferramentas" + File.separator + fileName);
+				}
+			}
+		}
+
 	}
 }
